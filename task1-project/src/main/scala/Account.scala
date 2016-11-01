1eab25662f39ac8d2f31b9ebaed3224a8a6c5838
Vemund M Santi <vemund@santi.no>
2016-11-01 12:00:48 +0100

    Pass 5 Account tests

--- task1-project/src/main/scala/Account.scala
+++ task1-project/src/main/scala/Account.scala
@@ -1,5 +1,30 @@
+import exceptions._
+
 class Account(initialBalance: Double, val uid: Int = Bank getUniqueId) {
-  def withdraw(amount: Double): Unit = ??? // Implement
-  def deposit(amount: Double): Unit = ??? // Implement
-  def getBalanceAmount: Double = ??? // Implement
+
+  private var balance = initialBalance;
+  def withdraw(amount: Double): Unit = {
+    if(!isPositive(amount)) {
+      throw new IllegalAmountException();
+    } else if (amount > this.balance) {
+      throw new NoSufficientFundsException();
+    } else {
+      this.balance -= amount
+    }
+  } // Implement
+
+  private def isPositive(number: Double): Boolean = {
+    return number > 0.0
+  }
+
+  def deposit(amount: Double): Unit = {
+    if(!isPositive(amount)) {
+      throw new IllegalAmountException();
+    } else {
+      this.balance += amount;
+    }
+  } // Implement
+  def getBalanceAmount: Double = {
+    return this.balance
+  } // Implement
 }
